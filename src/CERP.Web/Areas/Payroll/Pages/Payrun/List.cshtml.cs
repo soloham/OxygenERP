62058cb8af9071aad47b054d41d29b640b192609
monosoft54@gmail.com <Aipassword>
2020-03-27 19:55:52 +0500

    Grid Toolbar Changes

--- src/CERP.Web/Areas/Payroll/Pages/Payrun/List.cshtml.cs
+++ src/CERP.Web/Areas/Payroll/Pages/Payrun/List.cshtml.cs
@@ -87,7 +87,6 @@ namespace CERP.Web.Areas.Payroll.Pages.PayrunPage
                 GridLines = GridLine.Both,
                 SearchSettings = new GridSearchSettings() { },
                 //Toolbar = new List<object>() { "ExcelExport", "PdfExport", "CsvExport", "Print", "Search", "Delete", new { text = "Copy", tooltipText = "Copy", prefixIcon = "e-copy", id = "copy" }, new { text = "Copy With Header", tooltipText = "Copy With Header", prefixIcon = "e-copy", id = "copyHeader" } },
-                ContextMenuItems = new List<object>() { "AutoFit", "AutoFitAll", "SortAscending", "SortDescending", "Copy", "Edit", "Delete", "Save", "Cancel", "PdfExport", "ExcelExport", "CsvExport", "FirstPage", "PrevPage", "LastPage", "NextPage" },
                 Columns = GetSecondaryGridColumns()
             };
         }
